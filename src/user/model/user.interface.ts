8aaffa40d46444c8b630ce98396e5c7b3a580ad5
anhtuan <anhtuan6294@gmail.com>
2020-07-19 23:12:12 +0700

    User validate

--- src/user/model/user.interface.ts
+++ src/user/model/user.interface.ts
@@ -1,9 +1,17 @@
+import { IsEmail, IsNotEmpty, MinLength } from 'class-validator';
 export class User {
     id?: number;
     name?: string;
+
+    @MinLength(5)
     username?: string;
+    
+    @IsEmail()
     email?: string;
+
+    @IsNotEmpty()
     password?: string;
+    
     role?: UserRole;
 }
 
